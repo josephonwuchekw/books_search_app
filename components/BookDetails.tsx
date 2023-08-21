8caf971a8a7b67f976b95f5af5a090e3e30cfd35
josephonwuchekw <josephonwu0@gmail.com>
2023-08-21 10:58:40 -0400

    loading animation

--- components/BookDetails.tsx
+++ components/BookDetails.tsx
@@ -31,7 +31,7 @@ const BookDetails = ({ isOpen, closeModal, book }: BookDetailsProps) => {
           </Transition.Child>
 
           <div className="fixed inset-0 overflow-y-auto">
-            <div className="flex min-h-full items-center justify-center p-4 text-center">
+            <div className="flex min-h-full items-center justify-center p-1 text-center">
               <Transition.Child
                 as={Fragment}
                 enter="ease-out duration-300"
@@ -41,7 +41,7 @@ const BookDetails = ({ isOpen, closeModal, book }: BookDetailsProps) => {
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <Dialog.Panel className="relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white p-6 text-left shadow-xl transition-all flex flex-col gap-5">
+                <Dialog.Panel className="relative w-full max-w-[900px] max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white p-6 text-left shadow-xl transition-all flex flex-col gap-5">
                   <button
                     type="button"
                     className="absolute top-2 right-2 z-10 w-fit p-2 bg-primary-blue-100 rounded-full"
--- components/BooksGrid.tsx
+++ components/BooksGrid.tsx
@@ -74,9 +74,21 @@ const BooksGrid = ({ searchParams }: HomeProps) => {
 
   return (
     <>
+      {loading && (
+        <div className="home__error-container">
+          <div
+            className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+            role="status"
+          >
+            <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+              Loading...
+            </span>
+          </div>
+        </div>
+      )}
       {books && books.length > 0 ? (
         <section>
-          <div className="home__cars-wrapper">
+          <div className="home__books-wrapper">
             {books?.map((book) => (
               <BookCard book={book} key={book.id} />
             ))}
