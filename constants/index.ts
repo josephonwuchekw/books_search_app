161dfc7d6516d08452f4d1766015dd3a10e66833
josephonwuchekw <josephonwu0@gmail.com>
2023-08-22 15:13:11 -0400

    updated ec2 route

--- constants/index.ts
+++ constants/index.ts
@@ -2,9 +2,11 @@ import { OptionProps } from "@/types";
 
 const isDev = process && process.env.NODE_ENV === "development";
 
-export const base_url: string = isDev
-  ? "http://localhost:8000/api/v1"
-  : "http://13.59.3.245:8000/api/v1";
+// export const base_url: string = isDev
+//   ? "http://localhost:8000/api/v1"
+//   : "http://13.59.3.245:8000/api/v1";
+
+export const base_url: string = "http://13.59.3.245:8000/api/v1";
 
 export const books_route: string = "books";
 
