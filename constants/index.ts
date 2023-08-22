bf8c43d464defe978ea54e0fd7db91b9f9c49b15
josephonwuchekw <josephonwu0@gmail.com>
2023-08-22 15:09:02 -0400

    serving api from ec2

--- constants/index.ts
+++ constants/index.ts
@@ -4,7 +4,7 @@ const isDev = process && process.env.NODE_ENV === "development";
 
 export const base_url: string = isDev
   ? "http://localhost:8000/api/v1"
-  : "https://olsr3wsbgu6asn6mujjfkyf73q0obpfr.lambda-url.us-east-2.on.aws/api/v1";
+  : "http://13.59.3.245:8000/api/v1";
 
 export const books_route: string = "books";
 
